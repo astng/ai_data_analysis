c26366a17be11843d9bb942e0122c77815052e02
Pascal Sigel <pascal.sigel@gmail.com>
2019-10-03 11:49:43 -0300

    first try de feed forward, todo listo para sacar los datos y meternos a un keras

---
+++ apps/feed_forward_first_try.py
@@ -0,0 +1,45 @@
+import argparse
+import tensorflow as tf
+import pandas as pd
+import numpy as np
+
+data_len = 10
+
+
+def clean_data_used(data):
+    global data_len
+    if len(data) < 10:
+        return np.nan
+    else:
+        if pd.isna(data[0:5]).any():
+            return np.nan
+        return data[0:10]
+
+
+def decode_arrays(encoded_array):
+    return np.frombuffer(encoded_array.numpy(), dtype=np.float32)
+
+
+def decode_arrays_tf(tf_encoded):
+    [decode, ] = tf.py_function(decode_arrays, [tf_encoded], [np.float32])
+    return decode
+
+
+def main(dataset_file: str):
+    data_set_raw = pd.read_hdf(dataset_file, key='df')
+    data_set_raw = data_set_raw.map(clean_data_used).dropna()
+
+    dataset_cleaned_pandas = data_set_raw.map(lambda x: x[~np.isnan(x)].astype(np.float32).tobytes())
+    dataset_tf = tf.data.Dataset.from_tensor_slices(dataset_cleaned_pandas)
+    dataset_tf = dataset_tf.map(decode_arrays_tf)
+
+    for features in dataset_tf.take(100):
+        print('Features: ', features)
+        print(features.shape)
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--dataset_file', required=True, type=str)
+    cmd_args = parser.parse_args()
+    main(cmd_args.dataset_file)
