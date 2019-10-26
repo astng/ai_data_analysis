ac73da1598e618e41020e70daef7e022a6f7b4e8
miguel-a-solis <miguel.solis@innovacionyrobotica.com>
2019-10-26 04:18:42 -0300

    plotting iron evolution for diesel motor including LSC

---
+++ apps/component_type_motor.py
@@ -0,0 +1,59 @@
+import argparse
+import matplotlib.pyplot as plt
+import pandas as pd
+from pymongo import MongoClient
+
+time_horizon = 150
+n_components = 6
+
+
+def main(database: str, table: str, outfolder: str):
+    client = MongoClient()
+    db_mongo = client[database]
+    table_mongo = db_mongo[table]
+    query_fetch = table_mongo.find()
+    types = [39, 630, 681, 682] # 798, 799, 1236 and 1237 shows KeyError
+    components = {}
+    all_data = pd.DataFrame(query_fetch)
+    grouped_by_component_type = all_data.groupby(by='component_type')
+    for type in types:
+        component_group = grouped_by_component_type.groups[type]
+        results = all_data.iloc[component_group]["iron"].dropna().reset_index(drop=True)
+        components[type] = []
+        for id_component in set(all_data['component'][all_data['component_type'] == type]):
+            component_results = all_data["iron"][all_data['component'] == id_component].dropna().reset_index(drop=True)
+            components[type].append((len(component_results), id_component))
+    types.sort()
+    types.reverse()
+
+    for i in range(len(types)):
+        type = types[i]
+        components_to_plot = list(components[type])
+        components_to_plot.sort()
+        components_to_plot.reverse()
+        plt.figure()
+        legend = []
+        for _, id_component in components_to_plot[:n_components]:
+            component_results = all_data["iron"][all_data['component'] == id_component].dropna().reset_index(drop=True)
+            legend.append("id_component: " + str(id_component))
+            plt.plot(component_results[:time_horizon])
+            # limits = all_data.iloc[group][["ironLSC", "ironLSM", "ironLIC", "ironLIM"]].reset_index(drop=True)
+            limits = all_data["ironLSC"][all_data['component'] == id_component].dropna().reset_index(drop=True)
+            plt.plot(results[:time_horizon])
+            plt.plot(limits[:time_horizon], linestyle='dashed', label='_nolegend_')
+        plt.legend(legend)
+        plt.xlabel("muestras")
+        plt.title('Analisis del nivel de fierro para motor diesel (id_tipo_componente: ' + str(type) + ')')
+        plt.grid(True)
+        plt.savefig(outfolder + 'LSCdieselmotor' + str(type) + '.pdf', bbox_inches='tight')
+        plt.show()
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--database', type=str, required=True,
+                        help="MongoDB database used in essayed_results_database_raw_format.py")
+    parser.add_argument('--outfolder', type=str, default="../figures/iron-plots/")
+    parser.add_argument('--table', type=str, required=True, help="table used in essayed_results_database_raw_format.py")
+    cmd_args = parser.parse_args()
+    main(cmd_args.database, cmd_args.table, cmd_args.outfolder)
