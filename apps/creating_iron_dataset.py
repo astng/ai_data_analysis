a516ff20b38b18ac1f149fce56a6f53f7b0d390b
miguel-a-solis <miguel.solis@innovacionyrobotica.com>
2019-11-20 17:30:11 -0300

    generating dataset file with limits, lub-hours, comp, comp_type, machine_type

--- apps/creating_iron_dataset.py
+++ apps/creating_iron_dataset.py
@@ -18,13 +18,11 @@ def main(database: str, table: str, outfile: str):
 
     all_iron_component_series = list()
     for id_component, group in grouped_by_component:
-        aux = group[["component", "component_type", "machine_type", "ironLSC", "ironLSM", "iron"]].values
+        aux = group[["component", "component_type", "machine_type", "ironLSC", "ironLSM", "iron", "h_k_lubricante"]].values
         if pd.isna(aux).any():
             continue
         else:
             all_iron_component_series.append(aux[~np.isnan(aux)])
-        #normalized = (group["iron"] - group["iron"].mean())/group["iron"].std()
-        #all_iron_component_series.append(normalized.values)
     all_iron_component_series = pd.Series(all_iron_component_series)
     if not os.path.exists(os.path.dirname(outfile)):
         os.makedirs(os.path.dirname(outfile))
@@ -35,7 +33,7 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--database', type=str, required=True,
                         help="MongoDB database used in essayed_results_database_raw_format.py")
-    parser.add_argument('--outfile', type=str, default="../datasets/iron_dataset-idsandlimits.h5")
+    parser.add_argument('--outfile', type=str, default="../datasets/iron_dataset-whole.h5")
     parser.add_argument('--table', type=str, required=True, help="table used in essayed_results_database_raw_format.py")
     cmd_args = parser.parse_args()
     main(cmd_args.database, cmd_args.table, cmd_args.outfile)
