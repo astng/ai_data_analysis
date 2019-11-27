071c89c0468d3c9c9019d1cfdf268baaf9d54090
miguel-a-solis <miguel.solis@innovacionyrobotica.com>
2019-11-26 23:24:34 -0300

    including data normalisation

--- apps/lstm-with-categorical.py
+++ apps/lstm-with-categorical.py
@@ -14,7 +14,6 @@ def plot_history(history):
     ax2.set_ylabel('Mean Square Error')
     ax3.set_ylabel('Mean Abs Perc Error')
     fig.suptitle("LSTM with categorical data input")
-#    legends = []
     hist = pd.DataFrame(history.history)
     hist['epoch'] = history.epoch
     ax1.plot(hist['epoch'], hist['mae'])
@@ -23,14 +22,9 @@ def plot_history(history):
     ax2.plot(hist['epoch'], hist['val_mse'])
     ax3.plot(hist['epoch'], hist['mape'])
     ax3.plot(hist['epoch'], hist['val_mape'])
-#    legends.append('TrainError (size:' + str(training_size) + ')')
-#    legends.append('ValError (train_size:' + str(training_size) + ')')
     ax1.grid(True)
     ax2.grid(True)
     ax3.grid(True)
-#    ax1.legend(legends)
-#    ax2.legend(legends)
-#    ax3.legend(legends)
     plt.show()
 
 
@@ -51,35 +45,26 @@ def build_model(input_data, output_size, neurons=64, drop=0.25):
     return model
 
 
-def normalise_minmax(df):
-    return (df - df.min()) / (df.max() - df.min())
-
-
-def extract_window_data(df, window=10, normal=True):
+def extract_window_data(df, window=10):
     window_data = []
     for idx in range(len(df) - window):
         tmp = df[idx: (idx + window)].copy()
-        if normal:
-            tmp = normalise_minmax(tmp)
         window_data.append(tmp.values)
     return np.array(window_data)
 
 
-def prepare_data(df, target_col, window=10, normal=True):
+def prepare_data(df, target_col, window=10):
     train_data, test_data = train_test_split(df)
-    x_train = extract_window_data(train_data, window, normal)
-    x_test = extract_window_data(test_data, window, normal)
+    x_train = extract_window_data(train_data, window)
+    x_test = extract_window_data(test_data, window)
 
     y_train = train_data[target_col][window:].values
     y_test = test_data[target_col][window:].values
-    if normalise:
-        y_train = y_train / train_data[target_col][:-window].values - 1
-        y_test = y_test / test_data[target_col][:-window].values - 1
 
     return train_data, test_data, x_train, x_test, y_train, y_test
 
 
-np.random.seed(42)
+np.random.seed(420)
 
 window_len = 10
 test_size = 0.25
@@ -96,14 +81,16 @@ def main(dataset_file: str):
     data_set_raw = pd.DataFrame(data_set_raw.values.tolist())
     data_set_raw.columns = ["component", "component_type", "machine_type", "ironLSC", "ironLSM", "h_k_lubricante",
                             "iron"]
-    train, test, x_train, x_test, y_train, y_test = prepare_data(data_set_raw, "iron", window=window_len,
-                                                                 normal=normalise)
+    if normalise:
+        for col in data_set_raw.columns:
+            data_set_raw[col] = (data_set_raw[col] - data_set_raw[col].mean()) / data_set_raw[col].std()
+    train, test, x_train, x_test, y_train, y_test = prepare_data(data_set_raw, "iron", window=window_len)
 
     model = build_model(x_train, output_size=1, neurons=lstm_neurons, drop=dropout)
     logdir = "tensorboards_logs_lstm/scalars/whole_input-numerics"
     tensorboard_callback = keras.callbacks.TensorBoard(log_dir=logdir)
     history = model.fit(x_train, y_train, epochs=epochs, batch_size=batch_size, verbose=1, shuffle=True,
-                        callbacks=[tensorboard_callback])
+                        callbacks=[tensorboard_callback], validation_data=(x_test, y_test))
 
     plot_history(history)
 
