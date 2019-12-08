a30a40a416737bbcebb185cef448b430930ee20a
miguel-a-solis <miguel.solis@innovacionyrobotica.com>
2019-12-08 15:53:51 -0300

    including lub-changes prediction into dataset

--- apps/creating_iron_dataset.py
+++ apps/creating_iron_dataset.py
@@ -5,6 +5,7 @@ import pandas as pd
 from pymongo import MongoClient
 
 pd.set_option('display.max_columns', 50)
+pd.options.mode.chained_assignment = None
 
 
 def main(database: str, table: str, outfile: str):
@@ -14,15 +15,21 @@ def main(database: str, table: str, outfile: str):
     query_fetch = table_mongo.find()
 
     all_data = pd.DataFrame(query_fetch)
-    grouped_by_component = all_data.groupby(by='correlativo_muestra')
-
-    all_iron_component_series = list()
+    grouped_by_component = all_data.groupby(by='component')
     for id_component, group in grouped_by_component:
-        aux = group[["component", "component_type", "machine_type", "ironLSC", "ironLSM", "h_k_lubricante", "iron"]].values
+        predicted_changes = all_data[all_data['component'] == id_component]['iron'] < \
+                            0.6 * all_data[all_data['component'] == id_component]['iron'].shift()
+        all_data["change"][all_data["component"] == id_component] = predicted_changes.values
+    grouped_by_correlative = all_data.groupby(by='correlativo_muestra')
+    all_iron_component_series = list()
+    for correlativo, group in grouped_by_correlative:
+        aux = group[["change", "component", "component_type", "machine_type", "ironLSC", "ironLSM", "h_k_lubricante",
+                     "iron"]].values
         if pd.isna(aux).any():
             continue
         else:
-            all_iron_component_series.append(aux[~np.isnan(aux)])
+            #all_iron_component_series.append(aux[~np.isnan(aux[:, 1:])])
+            all_iron_component_series.append(aux[0])
     all_iron_component_series = pd.Series(all_iron_component_series)
     if not os.path.exists(os.path.dirname(outfile)):
         os.makedirs(os.path.dirname(outfile))
